d4c52d54d21b6a6c427e786c863254a1cd70752e
PETA Trainee <PETATrainee@outlook.com>
2021-09-03 10:27:25 +0100

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class batman
+    {
+        public void greeting()
+        {
+            Console.WriteLine("I'm Batman");
+        }
+    }
+
+    class villain
+    {
+        public void response()
+        {
+            Console.WriteLine("And I'm the Joker");
+        }
+
+        public virtual void overriddenResponse()
+        {
+            Console.WriteLine("And I'm the Joker");
+        }
+    }
+
+    class nonHuman : villain
+    {
+        public override void overriddenResponse()
+        {
+            Console.WriteLine("And I'm King Shark");
+        }
+
+        public new void response()
+        {
+            Console.WriteLine("And I'm King Croc");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            batman bruce = new batman();
+            bruce.greeting();
+
+            villain joker = new villain();
+            joker.response();
+
+            nonHuman Shark = new nonHuman();
+            Shark.overriddenResponse();
+
+            nonHuman Croc = new nonHuman();
+            Croc.response();
+        }
+    }
+}
